e5bdcb4237eb790093cc7337e0116a1dd2cdcbc8
Alix Fullerton <fullertonalix0@gmail.com>
2021-04-22 19:18:50 -0600

    Removing setup.py since it contains no unique info

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-Builds packages so that each package can be imported (and allow relative imports)
-
-"""
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup()
