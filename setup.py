0a09948855825341e92c35bef2c4c8cd59d5fb08
Alix Fullerton <fullertonalix0@gmail.com>
2021-04-22 19:06:16 -0600

    Working on new setup.py so version 1.0.3 can be published to pypi

--- setup.py
+++ setup.py
@@ -3,21 +3,7 @@
 Builds packages so that each package can be imported (and allow relative imports)
 
 """
-import dockerprettyps
 import setuptools
 
-setuptools.setup(
-    name="docker-pretty-ps",
-    version=dockerprettyps.__version__,
-    author="politeauthority",
-    author_email="fullteronalix0@gmail.com",
-    url="https://github.com/politeauthority/docker-pretty-ps",
-    download_url="https://github.com/politeauthority/docker-pretty-ps/archive/v0.0.1-alpha.zip",
-    packages=setuptools.find_packages(),
-    scripts=['dockerprettyps/bin/docker-pretty-ps'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-)
+if __name__ == "__main__":
+    setuptools.setup()
